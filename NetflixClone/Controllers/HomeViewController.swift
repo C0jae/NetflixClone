0fe02e5acba1908f30a4352ecc1fdbee5601e1fd
Youngjae Choi <cdw0807@naver.com>
2022-05-04 21:48:17 +0900

    22.05.04

--- NetflixClone/Controllers/HomeViewController.swift
+++ NetflixClone/Controllers/HomeViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // UITableView : 단일 열에 배열된 행을 사용하여 데이터를 표시하는 뷰
     private let homeFeedTable: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -20,22 +21,54 @@ class HomeViewController: UIViewController {
         view.backgroundColor = .systemBackground
         view.addSubview(homeFeedTable)
         
-        homeFeedTable.delegate = self
+        // delegate
+        //  객체 프로그래밍에 있어 하나의 객체가 모든 일을 처리하는게 아니라 처리하는 부분만 떼어내어
+        //  따로 객체로 만들어주고 그걸 넘기는(위임: Delegate)방법
+        homeFeedTable.delegate = self   // self(viewController)에서 homeFeedTable의 기능을 사용
         homeFeedTable.dataSource = self
+        
+        homeFeedTable.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
     }
     
     override func viewDidLayoutSubviews() {
+        // viewDidLayoutSubviews : 뷰가 서브 뷰의 배치를 완료했다는 소식을 뷰 컨트롤러에 전달한다.
         super.viewDidLayoutSubviews()
         homeFeedTable.frame = view.bounds   // bounds vs frame 차이 알아두기
     }
 }
 
+//extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        return 2
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 3
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.backgroundColor = .green
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 200
+//    }
+//
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        return 40
+//    }
+//}
+
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     
+    // 섹터의 수(없을 경우 하나의 섹터가 기본값)
     func numberOfSections(in tableView: UITableView) -> Int {
         return 20
     }
     
+    // 한 섹터안에 들어갈 셀의 수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
@@ -47,6 +80,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    // 셀 하나의 높이
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -14,7 +14,8 @@ class CollectionViewTableViewCell: UITableViewCell {
     private let collectionView: UICollectionView = {
         
         let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 140, height: 200)   // 셀의 크기 설정
+        layout.scrollDirection = .horizontal    // 스크롤 방향 설정 : 수평
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         return collectionView
@@ -32,17 +33,23 @@ class CollectionViewTableViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError();
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.frame = contentView.bounds
+    }
 }
 
 extension CollectionViewTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        cell.backgroundColor = .red
+        cell.backgroundColor = .green
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // 셀의 갯수
         return 10
     }
     

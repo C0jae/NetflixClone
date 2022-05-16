4314247a3c4a08747bc055493a1aced871ceb7b4
Youngjae Choi <cdw0807@naver.com>
2022-05-16 16:38:31 +0900

    22.05.16

--- NetflixClone/Resources/Extensions.swift
+++ NetflixClone/Resources/Extensions.swift
@@ -9,6 +9,8 @@ import Foundation
 
 extension String {
     func capitalizeFirstLetter() -> String {
+        // prefix(n) : n번째 숫자까지
+        // 첫글자는 대문자 + 나머지 글자는 소문자 설정
         return self.prefix(1).uppercased() + self.lowercased().dropFirst()
     }
 }
